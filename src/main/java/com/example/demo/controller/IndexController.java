3be1b0f9345f7fbdf83c3946bb94406044ef06d6
u_u <yu.ting375@gmail.com>
2025-06-01 02:19:18 +0800

    20250601_0219 前後端串接

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @RequestMapping(value = {"/", "/{path:^(?!api|parking-lots|images).*}/**"})
+    @RequestMapping(value = { "/", "/{path:[^\\.]*}" })
     public String forward() {
         return "forward:/index.html";
     }
