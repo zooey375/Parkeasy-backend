9d09e06947cbb5ac609600d8171f8ae1aba7175b
u_u <yu.ting375@gmail.com>
2025-06-03 16:26:01 +0800

    20250603_1625收藏停車場完成(尚未製作移除收藏)

---
+++ src/main/java/com/example/demo/WebConfig.java
@@ -0,0 +1,25 @@
+//因為跨來源請求(cors)被擋住，所以設定全域開放cors
+
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
