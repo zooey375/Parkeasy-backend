3bee1fa07777c24986acae98632fd164ef7fb335
zooey <yu.ting375@gmail.com>
2025-07-01 13:15:57 +0800

    微調

--- src/main/java/com/example/demo/service/CertService.java
+++ src/main/java/com/example/demo/service/CertService.java
@@ -3,14 +3,6 @@ package com.example.demo.service;
 import com.example.demo.model.dto.UserCert;
 
 public interface CertService {
-
-	/* 使用者登入驗證
-	 * @Param username 帳號
-	 * @Param password 密碼
-	 * @return 驗證成功後的使用者憑證資訊
-	 * @throws com.example.demo.exception.UserNotFoundException 使用者不存在
-     * @throws com.example.demo.exception.PasswordInvalidException 密碼錯誤
-	 */ 
 	
 	UserCert login(String username, String password);
 	
--- src/main/java/com/example/demo/service/EmailService.java
+++ src/main/java/com/example/demo/service/EmailService.java
@@ -1,4 +1,4 @@
-package com.example.demo.service;
+ package com.example.demo.service;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
